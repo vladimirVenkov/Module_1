648c6084b7509d75478b31e2af593a7269b0b9cf
vladimirVenkov <vladimir.ognyanov.venkov@gmail.com>
2018-05-31 11:36:23 +0300

    first test java file

---
+++ NewClassInModulOneTest.java
@@ -0,0 +1,22 @@
+package Modules.Module_1;
+
+import java.util.Scanner;
+
+public class NewClassInModulOneTest {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String number = scan.nextLine();
+        int n = 0;
+        for (int i = 0 ; i < number.length(); i ++){
+            char ch = number.charAt(i);
+            if (Character.isDigit(ch)){
+                n+= ch - '0';
+            }
+        }
+        //System.out.println(n);
+        while(n > 9){
+            n = n/10 + n%10;
+        }
+        System.out.println(n);
+    }
+}
